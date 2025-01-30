7e5df7174437c9423b7d3a1e2dbad7587205b3e4
meme-d0ge <MEME_D0GE@proton.me>
2025-01-30 21:27:19 +0100

    wrapped the header in a inner div

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,14 +5,16 @@ import React from "react";
 export default function Header() {
     return (
         <div>
-            <div className={'px-[25px] h-[70px] w-[100%]'}></div>
-            <header className={'fixed top-0 left-0 right-0 px-[25px] h-[80px] w-[100%] font-m bg-neutral-900 items-center flex gap-[15px] justify-between'}>
-                <nav className={'flex gap-[20px]'}>
-                    <Link className={'text-lightGray hover:text-white'} href={'/post'}>Create Post</Link>
-                    <Link className={'text-lightGray hover:text-white'} href={'/public'}>Public Posts</Link>
-                </nav>
-                <AuthMenu/>
+            <header className={'fixed top-0 left-0 right-0 bg-neutral-900'}>
+                <div className={'px-[20px] md:px-[40px] xl:px-[80px] h-[80px] font-m items-center flex gap-[15px] justify-between max-w-[1920px] mx-auto'}>
+                    <nav className={'flex gap-[20px]'}>
+                        <Link className={'text-lightGray hover:text-white'} href={'/post'}>Create Post</Link>
+                        <Link className={'text-lightGray hover:text-white'} href={'/public'}>Public Posts</Link>
+                    </nav>
+                    <AuthMenu/>
+                </div>
             </header>
+            <div className={'px-[25px] h-[70px] w-[100%]'}></div>
         </div>
     )
 }
