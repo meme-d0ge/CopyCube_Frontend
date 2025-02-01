f83dc8c9021c7fc4bbac9e1fe6a8785bb3636c0d
meme-d0ge <MEME_D0GE@proton.me>
2025-02-01 20:39:34 +0100

    addition of input logic, also button replacement

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,28 +1,33 @@
 'use client'
 import React, {useState} from "react";
 import TextInput from "@/UI/Inputs/TextInput/TextInput";
-import MatteButton from "@/UI/Buttons/MatteButton/MatteButton";
 import {SubmitHandler, useForm} from "react-hook-form";
-import {loginApi} from "@/api/auth/login-api";
+import {loginApi, LoginResponse} from "@/api/auth/login-api";
 import {ApiError} from "@/api/apiUtils";
 import {userStore} from "@/store/userStore";
 import {useRouter} from "next/navigation";
+import BlueMatteButton from "@/UI/Buttons/BlueMatteButton/BlueMatteButton";
 interface LoginData {
     username: string;
     password: string;
 }
 export default function LoginPage() {
     const {register, handleSubmit, setError, formState:{ errors }} = useForm<LoginData>({mode:'onTouched'})
-    const {initialize} = userStore(state => state);
+    const {initialize, setUser} = userStore(state => state);
     const [success, setSuccess] = useState(false)
     const router = useRouter()
     const onSubmit:SubmitHandler<LoginData> = async (data) => {
+        setUser({
+            loadingUser: true,
+        })
         const response = await loginApi({
             username: data.username,
             password: data.password,
         })
         if (response.code === 201) {
             setSuccess(true)
+            const newUser = response as LoginResponse;
+            setUser({user: newUser.user})
             initialize()
             setTimeout(() => {
                 router.push('/profile')
@@ -35,11 +40,15 @@ export default function LoginPage() {
                 message: responseError.message,
             })
         } else {
+            setSuccess(false)
             setError('username', {
                 type: 'text',
                 message: 'Network Error',
             })
         }
+        setUser({
+            loadingUser: false
+        })
     }
     return (
         <main className='px-[25px]'>
@@ -57,7 +66,7 @@ export default function LoginPage() {
                 </span>
                 <TextInput {...register("username", { required: 'Username field is required' })} type={'text'} nameField={'Userame'}/>
                 <TextInput {...register("password", { required: 'Password field is required' })} type={'password'} nameField={'Password'}/>
-                <MatteButton type="submit" className={'mt-[20px] w-[150px]'}>Login</MatteButton>
+                <BlueMatteButton type="submit" className={'mt-[20px] w-[150px]'}>Login</BlueMatteButton>
             </form>
         </main>
     );
